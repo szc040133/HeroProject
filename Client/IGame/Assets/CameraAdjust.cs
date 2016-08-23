6659c764f04278dfe436939bb85d2c02d513c0a4
szc040133 <1134208655@qq.com>
2016-08-23 19:18:31 +0800

    镜头调整

---
+++ Client/IGame/Assets/CameraAdjust.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAdjust : MonoBehaviour
+{
+    public Vector3 Position;
+    public Vector3 Direction = new Vector3(0, 5, 5);
+    public float Distance = 10;
+
+    private Transform _transform;
+	void Awake () {
+        _transform = transform;
+    }
+	
+	void Update () {
+        _transform.position = Position + Direction.normalized * Distance;
+        _transform.LookAt(Position);
+    }
+}
